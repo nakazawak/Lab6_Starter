bddf364b3b428b02ed75adb401b6898315f6e887
nakazawak <kojinakazawa129@gmail.com>
2026-05-13 18:27:01 -0700

    complete lab6

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -24,6 +24,7 @@ function getRecipesFromStorage() {
 	// A9. TODO - Complete the functionality as described in this function
 	//           header. It is possible in only a single line, but should
 	//           be no more than a few lines.
+	return JSON.parse(localStorage.getItem('recipes')) || [];
 }
 
 /**
@@ -35,10 +36,16 @@ function getRecipesFromStorage() {
  */
 function addRecipesToDocument(recipes) {
 	// A10. TODO - Get a reference to the <main> element
+	const main = document.querySelector('main');
 	// A11. TODO - Loop through each of the recipes in the passed in array,
 	//            create a <recipe-card> element for each one, and populate
 	//            each <recipe-card> with that recipe data using element.data = ...
 	//            Append each element to <main>
+	recipes.forEach((recipe) => {
+		const curElement = document.createElement('recipe-card');
+		curElement.data = recipe;
+		main.append(curElement);
+	});
 }
 
 /**
@@ -51,6 +58,7 @@ function saveRecipesToStorage(recipes) {
 	// B1. TODO - Complete the functionality as described in this function
 	//            header. It is possible in only a single line, but should
 	//            be no more than a few lines.
+	localStorage.setItem('recipes', JSON.stringify(recipes));
 }
 
 /**
@@ -76,4 +84,29 @@ function initFormHandler() {
 	// Steps B12 & B13 will occur inside the event listener from step B11
 	// B12. TODO - Clear the local storage
 	// B13. TODO - Delete the contents of <main>
+
+	// B2. Get a reference to the <form> element
+	const form = document.querySelector('form');
+
+	form.addEventListener('submit', (event) => {
+		event.preventDefault();
+
+		const formData = new FormData(form);
+		const recipeObject = Object.fromEntries(formData); 
+		const recipeCard = document.createElement('recipe-card');
+		recipeCard.data = recipeObject;
+		const main = document.querySelector('main');
+		main.append(recipeCard);
+		const recipes = getRecipesFromStorage();
+		recipes.push(recipeObject);
+		saveRecipesToStorage(recipes);
+	});
+	const clearButton = form.querySelector('button.danger');
+
+	clearButton.addEventListener('click', (event) => {
+		localStorage.clear();
+
+		const main = document.querySelector('main');
+		main.innerHTML = '';
+	});
 }
